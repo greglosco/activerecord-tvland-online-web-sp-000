554480c79b9af91345a43c8f86a6ad05544f64c9
Gregory Losco <gtlosco@gmail.com>
2020-01-19 22:19:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
+  
   def actors_list
     self.actors.collect do |actor|
       actor.full_name
