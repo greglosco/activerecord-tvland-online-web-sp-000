ae55605ecf9d5fcffa67780f2d0908bfbb3c9a3e
Gregory Losco <gtlosco@gmail.com>
2020-01-19 22:09:18 +0000

    Automatically backed up by Learn

--- db/migrate/20200119220445_add_catchphrase_to_characters.rb
+++ db/migrate/20200119220445_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[5.2]
   def change
-      add_column :name, :actor_id, :show_id, :catchphrase
+      add_column :characters, :catchphrase, :string
   end
 end
